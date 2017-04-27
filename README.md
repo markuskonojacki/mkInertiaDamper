1d04334cfd5615bba29460b8174b26afd78c7b4d
Markus Konojacki <markus@konojacki.de>
2017-04-27 14:39:07 +0200

    Readme + credits

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# mkInertiaDamper
+mkInertiaDamper
+=============
+
+
+What does it do?
+-------
+While flying stops the player almost immediately when not pressing any directional keys.
+
+
+Can I use this mod in my modpack?
+-------
+Yes, thanks to the power of the MIT licence you don't even have to ask!
--- src/main/java/it/slay/mkInertiaDamper/handlers/InertiaDamperEventHandler.java
+++ src/main/java/it/slay/mkInertiaDamper/handlers/InertiaDamperEventHandler.java
@@ -13,6 +13,7 @@ public class InertiaDamperEventHandler {
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
         EntityPlayer player = event.player;
 
+        // based on: https://github.com/brandon3055/Draconic-Evolution/blob/a3a19e32b486291084ac00e4e1dd10ef868d998a/src/main/java/com/brandon3055/draconicevolution/handlers/CustomArmorHandler.java#L269
         if (!player.onGround && player.capabilities.isFlying && player.moveForward == 0 && player.moveStrafing == 0 && (player.motionX != 0 || player.motionZ != 0)) {
             if (InertiaDamper.debug) {
                 System.out.println("X: " + player.motionX + " - Z: " + player.motionZ);
